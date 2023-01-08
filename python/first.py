2ef78cb735ee71417dd560570d37aac8327ca2ad
Salem Malick mohamed <saloumfall45@gmail.com>
2023-01-08 14:03:32 +0000

    take

--- python/first.py
+++ python/first.py
@@ -1,3 +1,21 @@
 print("hello this is first python program")
 print(14*60)
 print('hi')
+# from random import*
+# def jeu():
+#     dé=randint(1,6)
+#     if dé>4:
+#         a="gagné"
+#     else:
+#         a="perdu"
+#     return(dé,a)
+# print(dé)
+age = 15
+if(age>=18): 
+    print("you are not a teenager")
+else:
+    print("you are a teenager")
+print("ji")
+calcul = 5 / 8
+calcul = float(calcul)
+print(calcul)
